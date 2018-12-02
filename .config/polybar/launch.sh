b78cf09c128fd0fe34014d98f5a63f968e6c27ee
Goran Peretin <goran.peretin@gmail.com>
2018-12-02 08:58:12 -0800

    Show polybar on all monitors.

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,12 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch Polybar, using default config location ~/.config/polybar/config
-polybar top &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar top &
+  done
+else
+  polybar top &
+fi
 
 echo "Polybar launched..."
