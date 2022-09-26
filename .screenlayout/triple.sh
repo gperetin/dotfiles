55a2fbc8927889bacf1bd31926320bd4b5e1280c
Goran Peretin <goran.peretin@gmail.com>
2022-09-26 14:28:42 +0200

    Add script to setup triple screens

--- .screenlayout/triple.sh
+++ .screenlayout/triple.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DP-0 --mode 2560x1440 --pos 4000x0 --rotate right --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --output DP-3 --off --output HDMI-1 --off --output DP-4 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --output DP-5 --off
+xrandr --output DP-0 --mode 2560x1440 --pos 4000x0 --rotate right --rate 165.08 --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --rate 165.08 --output DP-3 --off --output HDMI-1 --off --output DP-4 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --rate 165.08 --output DP-5 --off
