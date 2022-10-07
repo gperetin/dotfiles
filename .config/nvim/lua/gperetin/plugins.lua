eec8bb87d769989398d3d9f89f4ab66dfa69221a
Goran Peretin <goran.peretin@gmail.com>
2022-10-07 09:15:49 +0200

    LSP diagnostics icons for neovim.

--- .config/nvim/lua/gperetin/plugins.lua
+++ .config/nvim/lua/gperetin/plugins.lua
@@ -329,6 +329,10 @@ globalstatus = false,
                         source = "always",  -- Or "if_many"
                     },
                 })
+                vim.fn.sign_define("DiagnosticSignError", {text="", texthl="DiagnosticSignError", numhl="DiagnosticSignError"})
+                vim.fn.sign_define("DiagnosticSignWarn", {text="", texthl="DiagnosticSignWarn", numhl="DiagnosticSignWarn"})
+                vim.fn.sign_define("DiagnosticSignHint", {text="", texthl="DiagnosticSignHint", numhl="DiagnosticSignHint"})
+                vim.fn.sign_define("DiagnosticSignInfo", {text="", texthl="DiagnosticSignInfo", numhl="DiagnosticSignInfo"})
 
             end,
             requires = {
