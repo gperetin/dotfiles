2d013acdde9594f4ac6ce4993095d26a72f7f0c2
Goran Peretin <goran.peretin@gmail.com>
2022-11-08 20:57:31 +0100

    Add autopairs and luasnip for neovim.

--- .config/nvim/lua/gperetin/plugins.lua
+++ .config/nvim/lua/gperetin/plugins.lua
@@ -223,6 +223,8 @@ globalstatus = false,
         use('ray-x/lsp_signature.nvim')
         use('onsails/lspkind.nvim')
 
+        use("L3MON4D3/LuaSnip")
+
         use({
             'hrsh7th/nvim-cmp',
             config = function()
@@ -235,6 +237,11 @@ globalstatus = false,
 
                 local cmp = require'cmp'
                 cmp.setup {
+                    snippet = {
+                        expand = function(args)
+                            require('luasnip').lsp_expand(args.body)
+                        end
+                    },
                     mapping = {
                         ['<C-j>'] = cmp.mapping.select_next_item(),
                         ['<C-k>'] = cmp.mapping.select_prev_item(),
@@ -269,6 +276,7 @@ globalstatus = false,
 
                     sources = cmp.config.sources({
                         { name = 'nvim_lsp' },
+                        { name = 'luasnip' },
                         { name = 'path' },
                         { name = 'buffer', keyword_length = 4 },
                     }),
@@ -301,10 +309,21 @@ globalstatus = false,
             end
         })
 
+        use {
+            "windwp/nvim-autopairs",
+            config = function()
+                require("nvim-autopairs").setup { }
+                local cmp_autopairs = require('nvim-autopairs.completion.cmp')
+                local cmp = require('cmp')
+                cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+            end
+        }
+
         use({
             'neovim/nvim-lspconfig',
             config = function()
-                local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+                -- local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+                local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
                 require('lspconfig')['pyright'].setup {
                     capabilities = capabilities
