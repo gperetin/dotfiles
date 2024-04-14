2af4a9f96bd7fcc6fa3548d11d98301b2652c547
Goran Peretin <goran.peretin@gmail.com>
2024-04-14 09:47:55 +0200

    Add xrandr script for nvidia drivers.

---
+++ .screenlayout/triple_nvidia_drivers.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+xrandr --output DP-0 --mode 2560x1440 --pos 4000x0 --rotate right --rate 165.08 --output DP-1 --off --output HDMI-0 --off --output DP-2 --mode 2560x1440 --pos 0x0 --rotate left --rate 165.08 --output DP-3 --off --output HDMI-1 --off --output DP-4 --primary --mode 2560x1440 --pos 1440x560 --rotate normal --rate 165.08 --output DP-5 --off
